e213720dd873d7fa8aad402b4b0858b3eb68b2bf
piyush <ps1089124@gmail.com>
2021-12-02 11:30:21 +0530

    piyush update

--- src/app/demo/dashboard/enquire/enquire.component.ts
+++ src/app/demo/dashboard/enquire/enquire.component.ts
@@ -7,6 +7,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 declare var $: any;
 
+// piyush
+
 @Component({
   selector: 'app-enquire',
   templateUrl: './enquire.component.html',
