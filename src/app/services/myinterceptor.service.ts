7cc0d923d6b212401f751fa7564fdb76b5d4a081
Ajay singh <er.ajaysingh1988@gmail.com>
2021-12-02 16:49:52 +0530

    anurag + kaushal+mohit = main resolve

--- src/app/services/myinterceptor.service.ts
+++ src/app/services/myinterceptor.service.ts
@@ -10,6 +10,7 @@ export class MyInterceptor implements HttpInterceptor {
         req = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + token) });
     }
     return next.handle(req);
+    
 }
 
 }
