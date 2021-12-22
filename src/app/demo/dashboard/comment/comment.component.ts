ce666bdaf4428cd1f5015b0b38202b0a99d45c71
Kaushal <myselfkaushal.kumar@gmail.com>
2021-12-22 13:14:05 +0530

    update

--- src/app/demo/dashboard/comment/comment.component.ts
+++ src/app/demo/dashboard/comment/comment.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MasterService } from 'src/app/services/master.service';
 
 @Component({
   selector: 'app-comment',
@@ -9,14 +10,55 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CommentComponent implements OnInit {
   id:any
+  blog:any=[]
+  loader:boolean=false
+  error:boolean=false;
+  message:any=""
 
-  constructor( private route: ActivatedRoute,private http: HttpClient, private router: Router) { }
+  constructor( private route: ActivatedRoute,private http: HttpClient, private router: Router, private master: MasterService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = params['id']; // (+) converts string 'id' to a number
     });
-    console.log(this.id);
+    this.getAllBlogComment()
+    
+  }
+  getAllBlogComment(){
+    this.loader=true
+    this.master.getMethod("/getBlogComments?id="+this.id).subscribe(data=>{
+      this.blog=data;
+      console.log(this.blog)
+      setTimeout(function(){
+        $('#example').DataTable();
+       }, 1000);
+       this.loader=false
+      
+    })
+  }
+
+  OnCheck(){
+
+  }
+  onDelete(ID){
+    this.master.deleteMethod("/deleteComment/"+ID).subscribe(data=>{
+      if(data['name']!='')
+      {    
+        this.error=true
+        this.message="Comment deleted successfully."
+        location.reload();
+  
+      }else{
+        this.error = true;
+        this.message = 'Failed to delete Comment!';
+        return false;
+      }
+  
+     
+     },(error=>{
+      alert("failed to delete comment something went wrong");
+     }))
+
   }
 
 }
